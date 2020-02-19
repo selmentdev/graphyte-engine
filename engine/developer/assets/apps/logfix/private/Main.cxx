74f86436e26dfa2372c48a87cc9328f6e9b063e9
Selmentdev <selmentdev@gmail.com>
2020-02-19 22:47:49 +0100

    [developer] logfix tool

---
+++ engine/developer/assets/apps/logfix/private/Main.cxx
@@ -0,0 +1,22 @@
+#include <Graphyte/Diagnostics.hxx>
+#include <Graphyte/CommandLine.hxx>
+#include <Graphyte/Modules.hxx>
+
+GX_DECLARE_LOG_CATEGORY(LogFixTool, Trace, Trace);
+GX_DEFINE_LOG_CATEGORY(LogFixTool);
+
+#include <Graphyte/Launch/Main.hxx>
+
+Graphyte::Application::ApplicationDescriptor GraphyteApp
+{
+    "Graphyte Assets Compiler",
+    "assets.compiler",
+    "Graphyte",
+    Graphyte::Application::ApplicationType::ConsoleTool,
+    Graphyte::Version{ 1, 0, 0, 0 }
+};
+
+int GraphyteMain([[maybe_unused]] int argc, [[maybe_unused]] char** argv) noexcept
+{
+    return 0;
+}
