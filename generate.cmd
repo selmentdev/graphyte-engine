ae07ee74adab47f8684638088083cfd0729d4e55
Selmentdev <selmentdev@gmail.com>
2020-12-27 23:05:31 +0100

    better syntax for generator

--- generate.cmd
+++ generate.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-dotnet run --project .\generate\Generate.csproj --configuration Debug -Profile:.\profiles\main2.jsonc -Platform:Windows -Generator:FastBuild -Toolchain:MSVC -OutputPath:".generated\fastbuild"
+dotnet run --project .\generate\Generate.csproj --configuration Debug -Profile:".\profiles\main2.jsonc" -Platform:"Windows" -Generator:"FastBuild" -Toolchain:"MSVC" -OutputPath:".generated\fastbuild" -NoLogo
 REM dotnet run --project .\generate\Generate.csproj --configuration Release -Profile:.\profiles\main2.jsonc -Platform:Windows -Generator:FastBuild -Toolchain:MSVC -OutputPath:".generated\fastbuild"
 
 REM tools\FastBuild\windows-x64\FBuild.exe -config ".generated\fastbuild/Graphyte-Windows-MSVC.bff" Target-MSVC-X64-Windows-GxBase-Debug
